404402340219c47b7039cbd3bacd5f7bf5918cbd
bradywales <93840984+bradywales@users.noreply.github.com>
2023-02-10 18:30:30 -0500

    passed linter

--- src/topics/searchTopics.js
+++ src/topics/searchTopics.js
@@ -3,15 +3,15 @@
 const db = require('../database');
 
 module.exports = function (Topics) {
-    Topics.searchTopics = async function (query, options) {
-        if (!query){
+    Topics.searchTopics = async function (query) {
+        if (!query) {
             return [];
         }
         query = String(query).toLowerCase();
-        let tidsTest = await db.getSortedSetRange(`topics:recent`, 0, -1);
-        let topicsTest = await Topics.getTopicsByTids(tidsTest, {});
+        const tidsTest = await db.getSortedSetRange(`topics:recent`, 0, -1);
+        const topicsTest = await Topics.getTopicsByTids(tidsTest, {});
         const res = topicsTest.filter(topic => topic.title.toLowerCase().includes(query) &&
-            topic.deleted != 1);
+            topic.deleted !== 1);
         return res;
     };
 };
