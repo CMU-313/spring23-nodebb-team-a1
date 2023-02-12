022f45a13ee59b99fbb825033543ad025339e5f0
Chloe <Chloe.deng1000@gmail.com>
2023-02-12 15:04:50 -0500

    Added interfaces and type annotations

--- src/topics/searchTopics.ts
+++ src/topics/searchTopics.ts
@@ -1,17 +1,21 @@
-'use strict';
+import db = require('../database');
 
-const db = require('../database');
+import { TopicObject } from '../types';
 
-module.exports = function (Topics) {
-    Topics.searchTopics = async function (query) {
+interface topic extends TopicObject {
+    search: (query: string) => object,
+    getTopicsByTids: (arg1: number, arg2: object) => TopicObject[],
+}
+
+export = function search(Topics: topic) {
+    Topics.search = async (query) => {
         if (!query) {
             return [];
         }
         query = String(query).toLowerCase();
-        const tidsTest = await db.getSortedSetRange(`topics:recent`, 0, -1);
+        const tidsTest = await db.getSortedSetRange(`topics:recent`, 0, -1) as number;
         const topicsTest = await Topics.getTopicsByTids(tidsTest, {});
-        const res = topicsTest.filter(topic => topic.title.toLowerCase().includes(query) &&
-            topic.deleted !== 1);
+        const res = topicsTest.filter(topic => topic.title.toLowerCase().includes(query));
         return res;
-    };
-};
+    }
+}
