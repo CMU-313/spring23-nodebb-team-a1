defa65f4632cbed4bd7110d47b952ada7bed2e54
bradywales <93840984+bradywales@users.noreply.github.com>
2023-03-01 21:34:03 -0500

    test cases for searchTopics

--- src/topics/searchTopics.js
+++ src/topics/searchTopics.js
@@ -1,7 +1,7 @@
 "use strict";
 const db = require("../database");
-module.exports = function search(Topics) {
-    Topics.search = async (query) => {
+module.exports = function searchTopics(Topics) {
+    Topics.searchTopics = async (query) => {
         if (!query) {
             return [];
         }
@@ -9,8 +9,8 @@ module.exports = function search(Topics) {
         // The next line calls a function in a module that has not been updated to TS yet
         // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
         const tidsTest = await db.getSortedSetRange(`topics:recent`, 0, -1);
-        const topicsTest = Topics.getTopicsByTids(tidsTest, {});
-        const res = topicsTest.filter(topic => topic.title.toLowerCase().includes(query));
+        const topicObjects = await Topics.getTopicsByTids(tidsTest, {});
+        const res = topicObjects.filter(topic => topic.title.toLowerCase().includes(query));
         return res;
     };
 };
